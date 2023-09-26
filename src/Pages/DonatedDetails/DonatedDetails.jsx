ed34236c9076d874da5a45ca8fc15aa1508b670f
Md Badrul Alam <developer.badrul@gmail.com>
2023-09-27 02:08:28 +0600

    Second Page Complete

--- src/Pages/DonatedDetails/DonatedDetails.jsx
+++ src/Pages/DonatedDetails/DonatedDetails.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 
 const DonatedDetails = ({ donated }) => {
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
+        <div className="card card-side bg-base-100 shadow-xl py-3">
             <figure><img src={donated.picture} alt="Movie" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{donated.title}</h2>
@@ -9,11 +10,34 @@ const DonatedDetails = ({ donated }) => {
                 <p>{donated.description.slice(0, 100)}</p>
                 <p>${donated.price}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Watch</button>
+                    <button className="btn btn-primary" onClick={() => document.getElementById('my_modal_5').showModal()}>View Details</button>
+
+                    
+
+                    {/* Open the modal using document.getElementById('ID').showModal() method */}
+                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                        <div className="modal-box text-justify">
+                            <h3 className="font-bold text-lg">Details:</h3>
+                            {/* {console.log()} */}
+                            <img className="mx-auto w-8/12" src={donated.picture} alt="" />
+                            <div className="flex gap-4 items-center">
+                            <h3 className="font-bold text-lg">Title:{donated.title}</h3>
+                            <div className="badge badge-secondary">{donated.category}</div>
+                            </div>
+                            <p className="py-4">{donated.description}</p>
+                            <p className="text-blue-400 font-bold">Price: ${donated.price}</p>
+                            <div className="modal-action">
+                                <form method="dialog">
+                                    {/* if there is a button in form, it will close the modal */}
+                                    <button className="btn btn-error">Close</button>
+                                </form>
+                            </div>
+                        </div>
+                    </dialog>
                 </div>
             </div>
         </div>
     );
 };
 
-export default DonatedDetails;
+export { DonatedDetails };
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import DonatedDetails from "../DonatedDetails/DonatedDetails";
+import { DonatedDetails } from "../DonatedDetails/DonatedDetails";
 
 const Donation = () => {
+
     const [showDonations, setShowDonations] = useState([]);
     const [noFound, setNofound] = useState("")
+    const [moreData, setMoreData] = useState(false)
     useEffect(() => {
         const getItem = JSON.parse(localStorage.getItem("donated"));
         if (getItem) {
@@ -13,17 +15,39 @@ const Donation = () => {
         }
     }, [])
 
-    console.log(showDonations);
+    const handleReset = () =>{
+        localStorage.clear();
+        setShowDonations([])
+        setNofound('No Donatation Add')
+    }
+
+    console.log(moreData);
+
     return (
-        <div>
-            <h1 className="text-xl">Donation</h1>
+
+        noFound ? <div className="text-center font-bold text-xl">No Donation Added</div> : <div>
+            <div className="flex justify-center items-center gap-3 font-semibold">
+                <h1 className="text-xl text-center font-bold">Your Donation List</h1>
+                <button className="bg-cyan-300 p-3 rounded-xl btn" onClick={handleReset}>Clear All</button>
+            </div>
             <ul className="grid grid-cols-1 md:grid-cols-2 gap-5 w-11/12 mx-auto">
                 {
-                    showDonations.map(x=> <DonatedDetails key={x.id} donated={x}></DonatedDetails>)
+                    moreData ? showDonations.map(x => <DonatedDetails key={x.id} donated={x}></DonatedDetails>)
+                    : showDonations.slice(0,4).map(x => <DonatedDetails key={x.id} donated={x}></DonatedDetails>)
                 }
             </ul>
+            {
+                 <div>
+                    <button className={`bg-cyan-300 p-3 rounded-xl btn mx-auto block mt-3 ${(showDonations.length > 4)} ? "block" : "hidden"`} onClick={()=> setMoreData(!moreData)}>{moreData ? "less Show" : "Show More"}</button>
+                 </div>
+            }
+
         </div>
+
+        
+
     );
+
 };
 
 export default Donation;
